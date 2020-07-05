bcfb5e0f0ae161d529b34daf8905c766259c549b
Shankhesh-16 <67553761+Shankhesh-16@users.noreply.github.com>
2020-07-05 16:17:47 +0530

    Update Password Generator.py

--- Password Generator.py
+++ Password Generator.py
@@ -2,7 +2,7 @@ import string
 import random
 
 character_set = string.punctuation + string.ascii_letters + string.digits
-
+# Replace length with number of letters of password you want
 def password_gen(length):
     password = ""
     for digit in range(length + 1):
